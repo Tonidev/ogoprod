f391fb1497f36540184fc27add00e2d812af84f1
Tonidev <nikto.com@gmail.com>
2017-03-13 19:39:28 +0300

    LOL2

--- index.php
+++ index.php
@@ -1,9 +1,23 @@
 <html>
 <head>
     <title>OGO-Prod.</title>
+<link rel="stylesheet" href="css/style.css">
+    <script src="js/jq.js"></script>
+    <script>
+        var backgi=1;
+        setInterval(function(){backg()},8000);
+        function backg()
+        {
+            backgi=backgi%5+1;
+            $("#start").animate({'opacity':'0'},800,function(){
+            $("#start").attr('src', 'backgrounds/'+backgi+'.jpg');
+            $("#start").animate({'opacity':'1'},800);});
+        }
+    </script>
+    <link rel="stylesheet" href="css/sweetalert.css">
+    <script src="js/sweetalert.min.js"></script>
 </head>
-<body>
-
+<body background="backgrounds/0.jpg">
+<img id="start" src="backgrounds/1.jpg" width="2000" height="1200" >
 </body>
 </html>
-
